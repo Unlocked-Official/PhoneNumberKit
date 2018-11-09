fdcc44d3cba3c04412a196d4dfebbe374670ad01
Tuan Nguyen <tuannguyenanh177@gmail.com>
2018-11-09 16:03:27 +0700

    update swift 4.2 (#1)
    * Added custom path ability
    
    * Changed passing path to passing data callback
    
    * Do not format text when the formatter is disabled
    
    * Added Codable to PhoneNumber
    
    * Build settings: switch to Swift 4.2 and update to modern build settings
    
    * Examples: update app delegate
    
    * Metadata updates for release 8.9.13
    
    * Tests: add test for new type of indian number
    
    * Metadta override: improve API
    
    * Metadata override: rename callback
    
    * Release 2.5.0
    
    * README update

--- PhoneNumberKit/UI/TextField.swift
+++ PhoneNumberKit/UI/TextField.swift
@@ -21,7 +21,7 @@ public class PhoneNumberTextField: UITextField, UITextFieldDelegate {
     /// Override setText so number will be automatically formatted when setting text by code
     override open var text: String? {
         set {
-            if newValue != nil {
+            if isPartialFormatterEnabled && newValue != nil {
                 let formattedNumber = partialFormatter.formatPartial(newValue! as String)
                 super.text = formattedNumber
             }
